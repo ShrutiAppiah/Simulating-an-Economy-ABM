491464ccac97e8f6a97646a7ab0f22dbed0fb13a
Shruti Appiah <shruti.appiah@gmail.com>
2017-03-21 19:41:38 -0400

    refactored and UI

--- Mechanism Design/VisualizeEconomy.py
+++ Mechanism Design/VisualizeEconomy.py
@@ -6,40 +6,55 @@ from mesa.visualization.TextVisualization import TextVisualization
 
 from WealthModel import WealthModel
 
-
+economy_scale = 15
 def agent_portrayal(agent):
-    portrayal = {"Shape": "circle",
+    """portrayal = {"Shape": "circle",
                  "Filled": "true",
-                 "r": 0.5}
+                 "r": 0.9}"""
+    
+    portrayal = {"Shape": "rect",
+                 "Filled": "true",
+                 "w": 1,
+                 "h": 1}
 
     if agent.wealth > 6:
         portrayal["Color"] = "#5cd65c"
         portrayal["Layer"] = 0
-        portrayal["r"] = 0.6
+        #portrayal["r"] = 0.6
+        portrayal["w"] = 0.6
+        portrayal["h"] = 0.6
     elif agent.wealth > 4:
         portrayal["Color"] = "#e6ffb3"
         portrayal["Layer"] = 0
-        portrayal["r"] = 0.4
+        #portrayal["r"] = 0.4
+        portrayal["w"] = 0.4
+        portrayal["h"] = 0.4
     elif agent.wealth > 2:
         portrayal["Color"] = "#ffff99"
         portrayal["Layer"] = 0
-        portrayal["r"] = 0.3
+        #portrayal["r"] = 0.3
+        portrayal["w"] = 0.3
+        portrayal["h"] = 0.3
     elif agent.wealth > 0:
         portrayal["Color"] = "#ffcc99"
         portrayal["Layer"] = 1
-        portrayal["r"] = 0.2
+        #portrayal["r"] = 0.2
+        portrayal["w"] = 0.2
+        portrayal["h"] = 0.2
     else:
         portrayal["Color"] = "#ff884d"
         portrayal["Layer"] = 2
-        portrayal["r"] = 0.1
+        #portrayal["r"] = 0.1
+        portrayal["w"] = 0.1
+        portrayal["h"] = 0.1
     return portrayal
 
-grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+grid = CanvasGrid(agent_portrayal, economy_scale, economy_scale, 650, 650)
 chart = ChartModule([
     {"Label": "Gini", "Color": "#6aa35e"}],
     data_collector_name='datacollector'
 )
 
-server = ModularServer(WealthModel, [grid, chart], "Wealth Model", 100, 10, 10)
+server = ModularServer(WealthModel, [grid, chart], "Wealth Model", economy_scale*economy_scale, economy_scale, economy_scale)
 server.port = 8521
 server.launch()
